342274722ced931dc9216b50da65e07ec3999109
Konadian <Konadian@192.168.0.16>
2019-04-18 10:32:01 -0400

    Added TwentyOneGame.java and some methods/wrote a bit of the game

---
+++ src/ca/sheridancollege/project/Hand.java
@@ -0,0 +1,32 @@
+package ca.sheridancollege.project;
+
+public class Hand {
+
+	private int cards;
+	private int handSize;
+
+	public Hand() {
+		// TODO - implement Hand.Hand
+		throw new UnsupportedOperationException();
+	}
+
+	public void showHand() {
+		// TODO - implement Hand.showHand
+		throw new UnsupportedOperationException();
+	}
+
+	public int getHandSize() {
+		return this.handSize;
+	}
+
+	public int getPoints() {
+		// TODO - implement Hand.getPoints
+		throw new UnsupportedOperationException();
+	}
+
+	public void addCard() {
+		// TODO - implement Hand.addCard
+		throw new UnsupportedOperationException();
+	}
+
+}
